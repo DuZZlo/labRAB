8ba0cb4c11a4aaf64321801f488acb316b81fa3e
DuZZlo <91115980+DuZZlo@users.noreply.github.com>
2021-10-12 09:44:28 +0300

    Update lab8_kuhtov_211_723.c

--- lab8_kuhtov_211_723.c
+++ lab8_kuhtov_211_723.c
@@ -1,48 +1,65 @@
 
 #include <stdio.h>
 
+
 int main()
 {
-    int a, A, B, temp;
-    //задание 1
-    printf("Enter size in bytes:\n");
-    scanf("%d", &a);
-    printf("= %d Kb\n\n", a/1024);
-    
-    //задание 2
-    do {
-    printf("Enter A, B (A>B):\n");
-    scanf("%d%d", &A, &B);
-    } while (B>=A || B<0 || A<0);
-    printf("B is %d times in A \n\n", A/B);
-    
-    //задание 3
-     do {
-    printf("Enter A, B (A>B):\n");
-    scanf("%d%d", &A, &B);
-    } while (B>=A || B<0 || A<0);
-    temp = A/B;
-    printf("space left - %d\n\n", A - temp*B);
-    
-    //задание 4
-    do {
-    printf("Enter a (2 signs):\n");
-    scanf("%d", &a);
-    } while (a/100!=0 || a/1<=9);
-    temp = a/10;
-    a = a - temp*10;
-    a = a*10 + temp;
-    printf("%d\n\n", a);
-    
-    //задание 5
-    do {
-    printf("Enter a (3 signs):\n");
-    scanf("%d", &a);
-    } while (a/1000!=0 || a/1<=99);
-    temp = a/100;
-    a = a - temp*100;
-    a = a*10;
-    a = a + temp;
-    printf("%d", a);
+    int a, A, B, temp, num;
+    int i=1;
+    while(i>0) {
+        printf("Enter task number:\n");
+        scanf("%d", &num);
+        switch (num) {
+        case 1:
+            //задание 1
+            printf("Enter size in bytes:\n");
+            scanf("%d", &a);
+            printf("= %d Kb\n\n", a/1024);
+            break;
+        case 2:
+            //задание 2
+            do {
+            printf("Enter A, B (A>B):\n");
+            scanf("%d%d", &A, &B);
+            } while (B>=A || B<0 || A<0);
+            printf("B is %d times in A \n\n", A/B);
+            break;
+        case 3:
+            //задание 3
+            do {
+            printf("Enter A, B (A>B):\n");
+            scanf("%d%d", &A, &B);
+            } while (B>=A || B<0 || A<0);
+            temp = A/B;
+            printf("space left - %d\n\n", A - temp*B);
+            break;
+        case 4:
+            //задание 4
+            do {
+            printf("Enter a (2 signs):\n");
+            scanf("%d", &a);
+            } while (a/100!=0 || a/1<=9);
+            temp = a/10;
+            a = a - temp*10;
+            a = a*10 + temp;
+            printf("%d\n\n", a);
+            break;
+        case 5:
+            //задание 5
+            do {
+            printf("Enter a (3 signs):\n");
+            scanf("%d", &a);
+            } while (a/1000!=0 || a/1<=99);
+            temp = a/100;
+            a = a - temp*100;
+            a = a*10;
+            a = a + temp;
+            printf("%d\n", a);
+            break;
+        default:
+            i-=1;
+            break;
+        }
+    }
     return 0;
 }
